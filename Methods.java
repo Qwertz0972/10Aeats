eb34f1e1295d830afe1728a7ce1c2c3c92749cec
Javoxir Iskandarov <origamingjavoxir@gmail.com>
2023-10-20 12:17:14 +0500

    first - 2

--- Methods.java
+++ Methods.java
@@ -1,5 +1,16 @@
+import java.util.Arrays;
+
 public class Methods {
-public static void Categories(){
+public static void categories(){
+    String []categories = {"1.All","2.Carry-out","3.Burgers","4.Sushi","5.Pizza","6.Wok","7.Pasta","8.Breakfasts","9.Lunch","10.Georgia","11.Italy","12.More"};
+    for (int i = 0;i< categories.length;i++){
+        System.out.println(categories[i]);
+    }
+}
+public static void more(){
+    String []more = {"1.Russian","2.Uzbek","3.Asia","4.Japan","5.China","6.Coffee","7.Deserts"};
+}
+public static void Carryout(){
 
 }
 }
--- Yandexeats.java
+++ Yandexeats.java
@@ -1,5 +1,7 @@
+
 public class Yandexeats {
     public static void main(String[] args) {
         System.out.println("Restaurants ");
+        Methods.categories();
     }
 }
